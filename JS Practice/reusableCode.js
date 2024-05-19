ab8f6425e39812bd32588d7ae86948ea77de692d
Tanmay K. Sawwalakhe <tkdsawwalakhe01@gmail.com>
2024-05-19 15:41:01 +0530

    adding file of reusable js code

---
+++ JS Practice/reusableCode.js
@@ -0,0 +1,25 @@
+const radius = [3 , 1, 2 ,4]
+
+
+// <----- use to calculate area
+const area = function(radius){
+    return Math.PI * radius * radius;
+}
+
+// <----- use to calculate circuference 
+
+const cicumference = function(radius){
+    return  Math.PI * Math.PI * radius
+}
+
+const calculate = function(radius , logic){
+    const output = []
+
+    for(let i = 0 ; i<radius.length ; i++){
+        output.push(logic(radius[i]))
+    }
+    return output
+}
+
+console.log(calculate(radius , area))
+console.log(calculate(radius , cicumference))
